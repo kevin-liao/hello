2e97b1eae92e4c0227128172da42902ce55cf591
Kevin Liao <liao@amazon.com>
2019-01-13 21:39:02 -0800

    [Hello] Initial commit after few years

---
+++ src/com/ll/hello/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2018
+ */
+package com.ll.hello;
+
+/**
+ * @author liao
+ */
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+    }
+
+}
